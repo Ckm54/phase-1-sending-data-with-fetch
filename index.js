76f7520ec9a8a7d7a8412e604099ad1cae8ac42b
Collins <mutaic84@gmail.com>
2022-04-15 16:43:46 +0300

    submit user data using post

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poddle",
+// }
+
+// const configurationObject = {
+//     // method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+// fetch(" http://localhost:3000/dogs", configurationObject)
+// .then(function(respunse){
+//     return respunse.json();
+// })
+// .then(function(object){
+//     console.log(object)
+// })
+// .catch(function(err){
+//     alert("Bad things! Ragnarok!");
+//     console.log(err.message)
+// })
+
+function submitData(userName, userEmail) {
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(userData => {
+        const id = userData.id
+        document.querySelector("body").innerHTML = `
+            ${id}
+        `
+    })
+    .catch(function(error){
+       document.querySelector("body").innerHTML = `
+            ${error.message}
+       `
+    })
+}
+
+
